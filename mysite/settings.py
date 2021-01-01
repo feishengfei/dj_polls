ecd78fa8298ed2b107e9f1f9449c4f72c2415970
zhouxiang <zhouxiang@twsz.com>
2021-01-02 01:04:23 +0800

    part 2 finished

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'n=xjjyi8qp*98n@57e6--plqi5-_^9qf+-f62sk(yl#wl=rr(b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
